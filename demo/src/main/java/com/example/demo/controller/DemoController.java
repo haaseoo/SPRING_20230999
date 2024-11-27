26d610ed40de7cb23f2a7375ac38ab1d264db3b1
haaseoo <sojun1098@naver.com>
2024-11-27 09:30:29 +0900

    4주차 완료

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,13 +1,21 @@
-package com.example.demo;
+package com.example.demo.controller;
 
-import java.time.LocalDate;
 
+import com.example.demo.model.service.TestService;
+import com.example.demo.model.domain.TestDB;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import java.time.LocalDate;
+import java.util.List;
 
 @Controller // 컨트롤러 어노테이션 명시
 public class DemoController {
+
+  @Autowired
+  TestService testService;
 
   @GetMapping("/hello") // 전송 방식 GET
   public String hello(Model model) {
@@ -30,5 +38,23 @@ public class DemoController {
   public String about_detailed(Model model) {
     return "about_detailed"; // hello.html 연결
   }
-  
+
+  @GetMapping("/test1")
+  public String thymeleaf_test1(Model model) {
+    model.addAttribute("data1", "<h2> 방갑습니다 </h2>");
+    model.addAttribute("data2", "태그의 속성 값");
+    model.addAttribute("link", 01);
+    model.addAttribute("name", "홍길동");
+    model.addAttribute("para1", "001");
+    model.addAttribute("para2", 002);
+    return "thymeleaf_test1";
+  }
+
+  @GetMapping("/testdb")
+  public String getAllTestDBs(Model model) {
+    List<TestDB> users = testService.findAll();
+    model.addAttribute("users", users);
+    // System.out.println("데이터 출력 디버그 : " + users);
+        return "testdb";
+    }
 }
